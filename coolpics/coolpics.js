7c0678c298b1491d10844e42c3c63865673374cb
makaylahansen <makaylahansen04@gmail.com>
2024-05-18 21:25:39 -0600

    test

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -23,3 +23,16 @@ window.addEventListener("resize", () => {
         document.querySelector(".menu").classList.add("hide");
     }
 });
+
+
+function handleResize() {
+    const menu = document.quierySelector(".menu");
+    if (window.innerWidth > 1000) {
+        menu.classList.remove("hide");
+    } else {
+        menu.classList.add("hide");
+    }
+}
+
+handleResize();
+window.addEventListener("resize", handleResize)
